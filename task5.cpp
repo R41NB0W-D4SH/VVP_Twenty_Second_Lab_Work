5cde0f28cd45d822796adbfa7aff42b846e6369c
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-01 22:26:16 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -1,1 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
 
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	ifstream ifile;
+	char c;
+	int k = 0;
+	bool marker = true;
+
+	ifile.open("file.txt");
+	while (!ifile.eof())
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			ifile.get(c);
+			if (c != ' ')
+			{
+				marker = false;
+			}
+		}
+		if (marker == true)
+		{
+			k++;
+		}
+		else
+		{
+			marker = true;
+		}
+		ifile.get(c);
+		if (c == '\n')
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				ifile.get(c);
+				if (c != ' ')
+				{
+					marker = false;
+				}
+			}
+			if (marker == true)
+			{
+				k++;
+			}
+			else
+			{
+				marker = true;
+			}
+		}
+	}
+	ifile.close();
+	cout << "Кол-во абзацев в тексте: " << k << endl;
+
+	system("pause");
+	return 0;
+}
