697cc883ff363420f7de56bc85a52d24379e47b9
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-12-01 20:57:05 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	ofstream ofile;
+	string file;
+	int n, k;
+
+	cout << "Введите имя файла: ";
+	getline(cin, file);
+	file += ".txt";
+	ofile.open(file);
+	cout << "Введите n, k: ";
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			ofile << "*";
+		}
+		ofile << "\n";
+	}
+
+	ofile.close();
+
+	cout << "Результат смотреть в файле file.txt в папке проекта " << endl;
+
+	system("pause");
+	return 0;
+}
